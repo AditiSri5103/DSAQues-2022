2ecea42ad169496d2ba37233b9603363cba12051
Aditi Srivastava <aditisrivastava7788@gmail.com>
2022-12-08 23:55:28 +0530

    8/12/22

--- reverse_vowels_string.java
+++ reverse_vowels_string.java
@@ -7,7 +7,7 @@ class Solution {
        i=0;
        j=n-1;
        char ch[] = s.toCharArray();
-        
+        System.out.println(v);
       while(i<j){
         if(!v.contains(s.charAt(i)))
         i++;
@@ -17,8 +17,10 @@ class Solution {
         char temp = ch[i];
         ch[i] = ch[j];
         ch[j] = temp;
+        i++;
+        j--;
         }
       }
-      return ch.toString();
+      return String.valueOf(ch);
     }
 }
