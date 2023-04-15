dcc1684effcaea8c3a193bbc6e8599d25d74e8cf
Aditi Srivastava <aditisrivastava7788@gmail.com>
2023-04-15 22:49:23 +0530

    15/04/23

---
+++ Stack/valid_parenthesis.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class valid_parenthesis {
+    public boolean isValid(String str) 
+    {
+    int i,n=str.length();
+        char ch;
+        Stack<Character> s=new Stack<>();
+        for(i=0;i<n;i++){
+            ch=str.charAt(i);
+            if(ch=='{'||ch=='['||ch=='(')
+             s.push(ch);
+            else if(ch=='}'||ch==']'||ch==')')
+            {
+                if(s.isEmpty())// condition when input has only closing brackets
+                    return false;
+                if((ch=='}'&&s.peek()=='{')||(ch==']'&&s.peek()=='[')||(ch==')'&&s.peek()=='('))  //checking existence of pairs
+                    s.pop();
+                else
+                    return false;
+            }
+            
+            }
+        if(s.isEmpty())
+       return true;
+        else
+            return false;
+        
+        
+    }
+}
